c8b5f3e3820e64da6af723d83b913ea4638e2434
everparadise <forever1314@sjtu.edu.cn>
2026-01-20 16:21:30 +0800

    dev: more scrips and config

--- long_run.sh
+++ long_run.sh
@@ -1,87 +1,35 @@
-BACKEND_CFG="/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/launch_vllm_dp8.toml"
-ROUTER_CFG="/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/vllm_router.toml"
+BACKEND_CFGS=(
+    # "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/throughput_vllm16.toml"
+    # "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/throughput_vllm32.toml"
+    # "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/throughput_vllm64.toml"
+    # "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/throughput_vllm1.toml"
+    # "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/throughput_vllm1_fanout4.toml"
+    "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/throughput_vllm2.toml"
+    # "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/throughput_vllm4.toml"
+    # "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/throughput_vllm8.toml"
+)
+ROUTER_CFG="/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/vllm_router_new_fullargs.toml"
 
 CLIENT_CFGS=(
     # "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/new_clients_6factor.toml"
-    "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/new_clients_6factor.toml"
+    "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/new_clients_single_request.toml"
     # "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/new_clients_5factor.toml"
     # "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/new_clients_7factor.toml"
 )
+CLIENT_CFG="/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/new_clients_single_request.toml"
 # POLICIES=("bailian-impl-q" "bounded-most-hit-q" "least-wait-token-q" "round-robin-q")
-# POLICIES=("bailian-impl-q" "round-robin-q")
-POLICIES=("round-robin-q")
-
-# WORK_DIR="/nvme/zkx/blitz-infer-pack"
-# VENV_PATH="/nvme/zkx/modified-vllm/myenv/bin/"
-
-# ./scripts/single_node/ipads_h20.sh \
-#     $BACKEND_CFG \
-#     $ROUTER_CFG \
-#     $CLIENT_CFG \
-#     "round-robin-q"
+# POLICIES=("join-shortest-q-ttft" "least-wait-token-mul-bs")
+POLICIES=("join-shortest-q-ttft, no-decode-prediction")
+# POLICIES=("least-wait-token-mul-bs")
 
 
-# For automatic end-to-end testing of all policies
-# for CLIENT_CFG in "${CLIENT_CFGS[@]}"; do
-#     export CLIENT_CFG
-#     for POLICY in "${POLICIES[@]}"; do
-#         ./scripts/single_node/ipads_h20.sh \
-#             $BACKEND_CFG \
-#             $ROUTER_CFG \
-#             $CLIENT_CFG \
-#             $POLICY
-#     done
-# done
 
-# ./scripts/single_node/ipads_h20_llama.sh \
-#     "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/dense_6simulator.toml" \
-#     $ROUTER_CFG \
-#     "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/new_clients_azure4_2.toml" \
-#     "round-robin-q"
-
-./scripts/single_node/ipads_h20.sh \
-    "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/launch_vllm_dp4_flashinfer.toml" \
-    "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/vllm_router_new.toml" \
-    "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/new_clients_single_request.toml" \
-    "join-shortest-q-ttft" # "round-robin-q"
-
-./scripts/single_node/ipads_llama.sh \
-    "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/launch_vllm_1.toml" \
-    "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/vllm_router_new.toml" \
-    "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/new_clients_single_request.toml" \
-    "round-robin-q"
-    
-
-# ./scripts/single_node/ipads_h20_llama.sh \
-#     "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/launch_vllm_dp8.toml" \
-#     $ROUTER_CFG \
-#     "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/new_clients_azure4_2.toml" \
-#     "round-robin-q"
-
-# ./scripts/single_node/human_in_the_loop.sh "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/launch_vllm_1.toml" \
-#     "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/vllm_router.toml" \
-#     "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/new_clients_single_request.toml" \
-#     "round-robin-q"
-#     --components "backend"
-
-
-# ./scripts/single_node/human_in_the_loop.sh "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/launch_vllm_1.toml" \
-#     "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/vllm_router_new.toml" \
-#     "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/new_clients_single_request.toml" \
-#     "join-shortest-q-ttft"
-#     --components "backend"
-
-# ./scripts/single_node/human_in_the_loop.sh "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/launch_vllm_1.toml" \
-#     "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/vllm_router_new.toml" \
-#     "/nvme/zkx/MetricsTestRunner/config/ipads-h20-1/new_clients_single_request.toml" \
-#     "join-shortest-q-ttft"
-#     --components "backend"
-# # For components start-up
-# FOR PART IN "backend" "router" "clients"; do
-# ${WORK_DIR}/MetricsTestRunner/scripts/single_node/human_in_the_loop.sh \
-#     $BACKEND_CFG \
-#     $ROUTER_CFG \
-#     $CLIENT_CFG \
-#     $POLICY \
-#     $PART
-# done 
+for BACKEND_CFG in "${BACKEND_CFGS[@]}"; do
+    for POLICY in "${POLICIES[@]}"; do
+        ./scripts/single_node/ipads_h20.sh \
+            $BACKEND_CFG \
+            $ROUTER_CFG \
+            $CLIENT_CFG \
+            $POLICY
+    done
+done
